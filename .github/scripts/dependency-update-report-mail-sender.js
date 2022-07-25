34e3c037cbb73773cca553dda24e248bd47ae085
Khalid Hamid <khalid64927@gmail.com>
2022-07-25 21:20:58 +0800

    Fix:WIP workflow files check

--- .github/scripts/dependency-update-report-mail-sender.js
+++ .github/scripts/dependency-update-report-mail-sender.js
@@ -7,14 +7,18 @@ module.exports = ({ }) => {
         port: 587,
         secureConnection: false,
         auth: {
-            user: `${process.env.MAIL_USERNAME}`, 
+            user: `${process.env.MAIL_USERNAME}`,
             pass: `${process.env.MAIL_PASSWORD}`
         },
         tls: {
             ciphers: 'SSLv3'
         }
     });
-    const report = require('fs').readFileSync('app/build/dependencyUpdates/report.txt', 'utf8')
+    path = require('path')
+    console.log("Current directory:", __dirname);
+    let reqPath = path.join(__dirname, '../../GitHubRepos/app/build/dependencyUpdates/report.html')
+    console.log("reqPath :", reqPath);
+    const report = require('fs').readFileSync(reqPath)
 
     const mailOptions = {
         from: {
@@ -23,7 +27,13 @@ module.exports = ({ }) => {
         },
         to: 'khalid64927@gmail.com', // Use your main account to get the email
         subject: 'Dependency update report of GitHubRepos ¯\\_(ツ)_/¯',
-        text: `${report}`
+        text: `${report}`,
+        attachments : [
+        {   // utf-8 string as an attachment
+                filename: 'build.gradle.kts',
+                path: reqPath
+            }
+        ]
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
